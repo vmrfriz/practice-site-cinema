ffdfe23255b61c6daf429bc08b5729589ad3bac4
Valeriy Grechukha <v.mrfriz@gmail.com>
2021-05-18 00:48:07 +0300

    Added .sql-file and db config example

---
+++ cinema.sql
@@ -0,0 +1,278 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Май 18 2021 г., 00:44
+-- Версия сервера: 10.4.12-MariaDB
+-- Версия PHP: 7.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `cinema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `actors`
+--
+
+CREATE TABLE `actors` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `photo_url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `actors`
+--
+
+INSERT INTO `actors` (`id`, `name`, `photo_url`) VALUES
+(1, 'Robert Downey Jr.', 'images/uploads/cast1.jpg'),
+(2, 'Chris Hemsworth', 'images/uploads/cast2.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `films`
+--
+
+CREATE TABLE `films` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `description` text NOT NULL,
+  `poster` varchar(255) NOT NULL,
+  `release_date` date NOT NULL,
+  `director` varchar(255) NOT NULL,
+  `duration` int(10) UNSIGNED NOT NULL,
+  `trailer_url` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `films`
+--
+
+INSERT INTO `films` (`id`, `name`, `description`, `poster`, `release_date`, `director`, `duration`, `trailer_url`, `created_at`) VALUES
+(1, 'Skyfall: Quantum of Spectre', 'Tony Stark creates the Ultron Program to protect the world, but when the peacekeeping program becomes hostile, The Avengers go into action to try and defeat a virtually impossible enemy together. Earth\'s mightiest heroes must come together once again to protect the world from global extinction.', '/images/uploads/movie-single.jpg', '2015-05-01', 'Joss Whedon', 141, 'https://www.youtube.com/embed/o-0hcF97wy0', '2021-05-17 18:59:19');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `films_actors`
+--
+
+CREATE TABLE `films_actors` (
+  `film_id` int(10) UNSIGNED NOT NULL,
+  `actor_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `films_actors`
+--
+
+INSERT INTO `films_actors` (`film_id`, `actor_id`) VALUES
+(1, 1),
+(1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `films_genres`
+--
+
+CREATE TABLE `films_genres` (
+  `film_id` int(10) UNSIGNED NOT NULL,
+  `genre_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `films_genres`
+--
+
+INSERT INTO `films_genres` (`film_id`, `genre_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `genres`
+--
+
+CREATE TABLE `genres` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `genres`
+--
+
+INSERT INTO `genres` (`id`, `title`) VALUES
+(1, 'Action'),
+(2, 'Sci-Fi'),
+(3, 'Adventure');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `media`
+--
+
+CREATE TABLE `media` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `film_id` int(10) UNSIGNED NOT NULL,
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `media`
+--
+
+INSERT INTO `media` (`id`, `film_id`, `url`) VALUES
+(1, 1, 'images/uploads/image11.jpg'),
+(2, 1, 'images/uploads/image21.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) UNSIGNED NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `privilege` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `privilege`) VALUES
+(1, 'admin', 'admin', 9);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `actors`
+--
+ALTER TABLE `actors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `films`
+--
+ALTER TABLE `films`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `films_actors`
+--
+ALTER TABLE `films_actors`
+  ADD UNIQUE KEY `unique_pair` (`film_id`,`actor_id`),
+  ADD KEY `actor_id` (`actor_id`);
+
+--
+-- Индексы таблицы `films_genres`
+--
+ALTER TABLE `films_genres`
+  ADD UNIQUE KEY `unique_pair` (`film_id`,`genre_id`),
+  ADD KEY `genre_id` (`genre_id`);
+
+--
+-- Индексы таблицы `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `media`
+--
+ALTER TABLE `media`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `film_id` (`film_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `actors`
+--
+ALTER TABLE `actors`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `films`
+--
+ALTER TABLE `films`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `media`
+--
+ALTER TABLE `media`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `films_actors`
+--
+ALTER TABLE `films_actors`
+  ADD CONSTRAINT `films_actors_ibfk_1` FOREIGN KEY (`actor_id`) REFERENCES `actors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `films_actors_ibfk_2` FOREIGN KEY (`film_id`) REFERENCES `films` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `films_genres`
+--
+ALTER TABLE `films_genres`
+  ADD CONSTRAINT `films_genres_ibfk_1` FOREIGN KEY (`film_id`) REFERENCES `films` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `films_genres_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `media`
+--
+ALTER TABLE `media`
+  ADD CONSTRAINT `media_ibfk_1` FOREIGN KEY (`film_id`) REFERENCES `films` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ config/db.example.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'user' => 'username',
+    'pass' => 'password',
+    'base' => 'database',
+];
