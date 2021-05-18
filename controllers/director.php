7c79367432a77399517bdf83a743362ec7df84d7
Valeriy Grechukha <v.mrfriz@gmail.com>
2021-05-19 00:17:22 +0300

    Removed extra controllers

--- controllers/director.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$director = Router::getNext();
-
-require_once $_SERVER['DOCUMENT_ROOT'] . '/view/header.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/view/search.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/view/footer.php';
--- controllers/search.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once $_SERVER['DOCUMENT_ROOT'] . '/view/header.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/view/search.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/view/footer.php';
