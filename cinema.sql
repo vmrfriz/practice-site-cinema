67046d4732dac182f731e4a838f68ed97546c63a
Valeriy Grechukha <v.mrfriz@gmail.com>
2021-05-20 01:58:04 +0300

    Updated database file

--- cinema.sql
+++ cinema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 19 2021 г., 01:22
+-- Время создания: Май 20 2021 г., 01:57
 -- Версия сервера: 10.4.12-MariaDB
 -- Версия PHP: 7.2.29
 
@@ -24,29 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `actors`
---
-
-CREATE TABLE `actors` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `photo_url` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `actors`
---
-
-INSERT INTO `actors` (`id`, `name`, `photo_url`) VALUES
-(1, 'Robert Downey Jr.', 'images/uploads/cast1.jpg'),
-(2, 'Chris Hemsworth', 'images/uploads/cast2.jpg'),
-(3, 'Scarlett Johansson', ''),
-(4, 'Adam Driver', ''),
-(5, 'Laura Dern', '');
-
--- --------------------------------------------------------
-
---
 -- Структура таблицы `films`
 --
 
@@ -56,7 +33,7 @@ CREATE TABLE `films` (
   `description` text NOT NULL,
   `poster` varchar(255) NOT NULL,
   `release_date` date NOT NULL,
-  `director` varchar(64) NOT NULL,
+  `director_id` int(10) UNSIGNED NOT NULL,
   `duration` int(10) UNSIGNED NOT NULL,
   `trailer_url` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL
@@ -66,9 +43,10 @@ CREATE TABLE `films` (
 -- Дамп данных таблицы `films`
 --
 
-INSERT INTO `films` (`id`, `name`, `description`, `poster`, `release_date`, `director`, `duration`, `trailer_url`, `created_at`) VALUES
-(1, 'Skyfall: Quantum of Spectre', 'Tony Stark creates the Ultron Program to protect the world, but when the peacekeeping program becomes hostile, The Avengers go into action to try and defeat a virtually impossible enemy together. Earth\'s mightiest heroes must come together once again to protect the world from global extinction.', '/images/uploads/movie-single.jpg', '2015-05-01', 'Joss Whedon', 141, 'https://www.youtube.com/embed/o-0hcF97wy0', '2021-05-17 18:59:19'),
-(2, 'Marriage Story', 'Marriage Story is a 2019 British-American drama film written and directed by Noah Baumbach, who produced the film with David Heyman. It stars Scarlett Johansson and Adam Driver, with Laura Dern, Alan Alda, Ray Liotta, Julie Hagerty, and Merritt Wever in supporting roles. The film follows a married couple, an actress and a stage director (Johansson and Driver), going through a coast-to-coast divorce. ', '/images/uploads/MarriageStoryPoster.png', '2019-11-15', 'Noah Baumbach', 136, '', '2021-05-19 20:56:45');
+INSERT INTO `films` (`id`, `name`, `description`, `poster`, `release_date`, `director_id`, `duration`, `trailer_url`, `created_at`) VALUES
+(1, 'Skyfall: Quantum of Spectre', 'Tony Stark creates the Ultron Program to protect the world, but when the peacekeeping program becomes hostile, The Avengers go into action to try and defeat a virtually impossible enemy together. Earth\'s mightiest heroes must come together once again to protect the world from global extinction.', '/images/uploads/movie-single.jpg', '2015-05-01', 6, 141, 'https://www.youtube.com/embed/o-0hcF97wy0', '2021-05-17 18:59:19'),
+(2, 'Marriage Story', 'Marriage Story is a 2019 British-American drama film written and directed by Noah Baumbach, who produced the film with David Heyman. It stars Scarlett Johansson and Adam Driver, with Laura Dern, Alan Alda, Ray Liotta, Julie Hagerty, and Merritt Wever in supporting roles. The film follows a married couple, an actress and a stage director (Johansson and Driver), going through a coast-to-coast divorce. ', '/images/uploads/MarriageStoryPoster.png', '2019-11-15', 7, 136, '', '2021-05-19 20:56:45'),
+(3, 'Форсаж 9', 'Доминик Торетто ведет спокойную жизнь вместе с Летти и своим сыном Брайаном, однако, они знают, что новая опасность всегда где-то рядом. В этот раз Доминику придется встретиться с призраками прошлого, если он хочет спасти самых близких. Команда снова собирается вместе, чтобы предотвратить дерзкий план по захвату мира, который придумал самый опасный преступник и безбашенный водитель из всех, с кем они сталкивались ранее. Ситуация усложняется тем, что этот человек — брат Доминика Джейкоб, которого много лет назад изгнали из семьи.', '/images/uploads/fast-and-furious.webp', '2021-05-19', 1, 145, '', '2021-05-19 20:02:52');
 
 -- --------------------------------------------------------
 
@@ -78,14 +56,14 @@ INSERT INTO `films` (`id`, `name`, `description`, `poster`, `release_date`, `dir
 
 CREATE TABLE `films_actors` (
   `film_id` int(10) UNSIGNED NOT NULL,
-  `actor_id` int(10) UNSIGNED NOT NULL
+  `human_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `films_actors`
 --
 
-INSERT INTO `films_actors` (`film_id`, `actor_id`) VALUES
+INSERT INTO `films_actors` (`film_id`, `human_id`) VALUES
 (1, 1),
 (1, 2),
 (2, 3),
@@ -137,22 +115,67 @@ INSERT INTO `genres` (`id`, `title`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `humans`
+--
+
+CREATE TABLE `humans` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `photo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `humans`
+--
+
+INSERT INTO `humans` (`id`, `name`, `photo`) VALUES
+(1, 'Robert Downey Jr.', '/images/uploads/cast1.jpg'),
+(2, 'Chris Hemsworth', '/images/uploads/cast2.jpg'),
+(3, 'Scarlett Johansson', ''),
+(4, 'Adam Driver', ''),
+(5, 'Laura Dern', ''),
+(6, 'Joss Whedon', ''),
+(7, 'Noah Baumbach', '');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `media`
 --
 
 CREATE TABLE `media` (
   `id` int(10) UNSIGNED NOT NULL,
   `film_id` int(10) UNSIGNED NOT NULL,
-  `url` varchar(255) NOT NULL
+  `url` varchar(255) NOT NULL,
+  `thumb` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `media`
 --
 
-INSERT INTO `media` (`id`, `film_id`, `url`) VALUES
-(1, 1, 'images/uploads/image11.jpg'),
-(2, 1, 'images/uploads/image21.jpg');
+INSERT INTO `media` (`id`, `film_id`, `url`, `thumb`) VALUES
+(1, 1, '/images/uploads/image11.jpg', '/images/uploads/image1.jpg'),
+(2, 1, '/images/uploads/image21.jpg', '/images/uploads/image2.jpg'),
+(3, 1, '/images/uploads/image31.jpg', '/images/uploads/image3.jpg'),
+(4, 1, '/images/uploads/image41.jpg', '/images/uploads/image4.jpg'),
+(5, 1, '/images/uploads/image51.jpg', '/images/uploads/image5.jpg'),
+(6, 1, '/images/uploads/image61.jpg', '/images/uploads/image6.jpg'),
+(7, 1, '/images/uploads/image71.jpg', '/images/uploads/image7.jpg'),
+(8, 1, '/images/uploads/image81.jpg', '/images/uploads/image8.jpg'),
+(9, 1, '/images/uploads/image91.jpg', '/images/uploads/image9.jpg'),
+(10, 1, '/images/uploads/image101.jpg', '/images/uploads/image10.jpg'),
+(11, 1, '/images/uploads/image111.jpg', '/images/uploads/image1-1.jpg'),
+(12, 1, '/images/uploads/image121.jpg', '/images/uploads/image12.jpg'),
+(13, 1, '/images/uploads/image131.jpg', '/images/uploads/image13.jpg'),
+(14, 1, '/images/uploads/image141.jpg', '/images/uploads/image14.jpg'),
+(15, 1, '/images/uploads/image151.jpg', '/images/uploads/image15.jpg'),
+(16, 1, '/images/uploads/image161.jpg', '/images/uploads/image16.jpg'),
+(17, 1, '/images/uploads/image171.jpg', '/images/uploads/image17.jpg'),
+(18, 1, '/images/uploads/image181.jpg', '/images/uploads/image18.jpg'),
+(19, 1, '/images/uploads/image191.jpg', '/images/uploads/image19.jpg'),
+(20, 1, '/images/uploads/image201.jpg', '/images/uploads/image20.jpg'),
+(21, 1, '/images/uploads/image211.jpg', '/images/uploads/image2-1.jpg');
 
 -- --------------------------------------------------------
 
@@ -179,23 +202,18 @@ INSERT INTO `users` (`id`, `login`, `password`, `privilege`) VALUES
 --
 
 --
--- Индексы таблицы `actors`
---
-ALTER TABLE `actors`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Индексы таблицы `films`
 --
 ALTER TABLE `films`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `films_ibfk_1` (`director_id`);
 
 --
 -- Индексы таблицы `films_actors`
 --
 ALTER TABLE `films_actors`
-  ADD UNIQUE KEY `unique_pair` (`film_id`,`actor_id`),
-  ADD KEY `actor_id` (`actor_id`);
+  ADD UNIQUE KEY `unique_pair` (`film_id`,`human_id`),
+  ADD KEY `actor_id` (`human_id`);
 
 --
 -- Индексы таблицы `films_genres`
@@ -208,6 +226,12 @@ ALTER TABLE `films_genres`
 -- Индексы таблицы `genres`
 --
 ALTER TABLE `genres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `humans`
+--
+ALTER TABLE `humans`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -228,16 +252,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT для таблицы `actors`
---
-ALTER TABLE `actors`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
 -- AUTO_INCREMENT для таблицы `films`
 --
 ALTER TABLE `films`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `genres`
@@ -246,10 +264,16 @@ ALTER TABLE `genres`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT для таблицы `humans`
+--
+ALTER TABLE `humans`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT для таблицы `media`
 --
 ALTER TABLE `media`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
@@ -262,10 +286,16 @@ ALTER TABLE `users`
 --
 
 --
+-- Ограничения внешнего ключа таблицы `films`
+--
+ALTER TABLE `films`
+  ADD CONSTRAINT `films_ibfk_1` FOREIGN KEY (`director_id`) REFERENCES `humans` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Ограничения внешнего ключа таблицы `films_actors`
 --
 ALTER TABLE `films_actors`
-  ADD CONSTRAINT `films_actors_ibfk_1` FOREIGN KEY (`actor_id`) REFERENCES `actors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `films_actors_ibfk_1` FOREIGN KEY (`human_id`) REFERENCES `humans` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `films_actors_ibfk_2` FOREIGN KEY (`film_id`) REFERENCES `films` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
