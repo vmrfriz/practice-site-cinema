b032759802b81979fde584cd4b5ddd4728a176fb
Valeriy Grechukha <v.mrfriz@gmail.com>
2021-05-20 12:09:39 +0300

    Added php version checker before run scripts

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+if (floatval(phpversion()) < 7.1) {
+    die('Для работы сайта необходима версия PHP между 7.1 и 7.4 включительно.');
+}
+
 require_once 'classes/autoload.php';
 
 Router::route($_SERVER['REQUEST_URI']);
