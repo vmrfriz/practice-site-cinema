80444624e7d7a5b7e6c8b0e72de57cc277ace0e9
Valeriy Grechukha <v.mrfriz@gmail.com>
2021-05-25 09:33:03 +0300

    Useless /admin/ deleted, README.md added

--- admin/js/main.js
+++
@@ -1,5 +0,0 @@
-import { delete_item } from './delete/delete.js';
-import { file } from "./file/file.js";
-
-delete_item();
-file();
--- admin/module/delete_item.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    require($_SERVER["DOCUMENT_ROOT"] . '/admin/config/db.php');
-
-    $id = $_POST['id'];
-    $item = $_POST['item'];
-
-    $delete = delete($conn,"DELETE FROM $item WHERE id = '$id'");
